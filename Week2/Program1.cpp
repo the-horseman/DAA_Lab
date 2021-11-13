49e7e26235e97f88919844f794238ae90ee8d02b
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2021-11-14 03:18:29 +0530

    stl removed

--- Week2/Program1.cpp
+++ Week2/Program1.cpp
@@ -1,39 +1,63 @@
 #include <bits/stdc++.h>
 
-#define vi vector<int>
-#define ll long long
 using namespace std;
+
+int binary_search(int a[], int l, int key, bool find_first_occurence)
+{
+    int b = 0, e = l - 1;
+    while (b <= e)
+    {
+        int mid = (b + e) / 2;
+        if (a[mid] == key)
+        {
+            if (find_first_occurence)
+            {
+                if (mid == 0 || a[mid - 1] < a[mid])
+                    return mid;
+                else
+                    e = mid - 1;
+            }
+            else
+            {
+                if (mid == l - 1 || a[mid + 1] > a[mid])
+                    return mid;
+                else
+                    b = mid + 1;
+            }
+        }
+        else if (a[mid] > key)
+            e = mid - 1;
+        else
+            b = mid + 1;
+    }
+    return -1;
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int no;
-    cin >> no;
-    for (int f = 0; f < no; f++)
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    for (int z = 0; z < n; ++z)
     {
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-        int n;
-        cin >> n;
-        vi arr(n);
-        for (int i = 0; i < n; ++i)
-            cin >> arr[i];
-        int c;
-        cin >> c;
-        auto x = lower_bound(arr.begin(), arr.end(), c);
-        auto y = lower_bound(arr.begin(), arr.end(), c + 1);
-        int val = x - arr.begin();
-        int val1 = y - arr.begin();
-        if (val != arr.size() && arr[val] == c)
+        int l;
+        cin >> l;
+        int a[l];
+        for (int i = 0; i < l; ++i)
+            cin >> a[i];
+        int key;
+        cin >> key;
+        int first_occurence = binary_search(a, l, key, true);
+        if (first_occurence >= 0)
         {
-            cout << c << " - " << (val1 - val) << endl;
+            int last_occurence = binary_search(a, l, key, false);
+            cout << key << " - " << last_occurence - first_occurence + 1 << endl;
+            continue;
         }
-        else
-        {
-            cout<<"Key not present"<<endl;
-        }
+        cout << "Key Not Found" << endl;
     }
-
     return 0;
 }
